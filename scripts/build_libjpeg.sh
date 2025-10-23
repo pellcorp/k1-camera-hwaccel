996d10e6d16c391dedb197f05d22da823c76f44f
Jason Pell <jason@pellcorp.com>
2025-10-24 08:52:37 +1100

    change build to use local submodule / subtree

--- scripts/build_libjpeg.sh
+++ scripts/build_libjpeg.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd $SOURCE_DIR
 
-if [ ! -d "jpeg-9d" ]; then
-    wget "https://www.ijg.org/files/jpegsrc.v9d.tar.gz"
-    tar -zxf "jpegsrc.v9d.tar.gz"
-    rm "jpegsrc.v9d.tar.gz"
-fi
-
 cd "jpeg-9d"
 
 ./configure --host=x86_64 --build=mips --prefix=$BUILD_PREFIX
--- scripts/build_libsynchronous-frames.sh
+++ scripts/build_libsynchronous-frames.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd $SOURCE_DIR
 
-if [ ! -d "libsynchronous-frames" ]; then
-    wget "https://git.zevaryx.com/zevaryx/libsynchronous-frames/archive/main.tar.gz" -O "libsynchronous-frames.tar.gz"
-    tar -zxf "libsynchronous-frames.tar.gz"
-    rm "libsynchronous-frames.tar.gz"
-fi
-
 cd "libsynchronous-frames/"
 
 mkdir -p _build && cd _build
